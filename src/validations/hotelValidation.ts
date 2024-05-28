f68af8787cf8b137b93a1e3c0a55a3f717d7b9ac
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-05-28 20:03:31 +0530

    removed numAdults and numChildren from hotel model

--- src/validations/hotelValidation.ts
+++ src/validations/hotelValidation.ts
@@ -6,8 +6,6 @@ export const hotelCreationValidationRules = [
     check('hotelType', 'Hotel Type is required').not().isEmpty(),
     check('country', 'Country is required').not().isEmpty(),
     check('city', 'City is required').not().isEmpty(),
-    check('numOfAdults', 'Number of adults is required and must be an integer').isInt({min: 1}),
-    check('numOfChildren', 'Number of children is required and must be an integer').isInt({min: 0}),
     check('facilities', 'Facilities must be an array').isArray(),
     check('pricePerNight', 'Price per night is required and must be a positive number').isFloat({min: 0}),
     check('rating', 'Rating must be a number between 0 and 5').isFloat({min: 0, max: 5}),
