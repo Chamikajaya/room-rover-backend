71a34f19f748da08e0dd612c3a10314caec1d39b
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-06-07 16:04:56 +0530

    commiting before backend deploy

--- src/routes/bookingsRouter.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const validateCookie_1 = require("../middleware/validateCookie");
-const bookingsController_1 = require("../controllers/bookingsController");
-const bookingsRouter = (0, express_1.Router)();
-bookingsRouter.get("/", validateCookie_1.validateCookie, bookingsController_1.getMyBookings);
-exports.default = bookingsRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,41 +3,46 @@ import express from "express";
 import cors from "cors";
 import userRouter from "./routes/userRouters";
 import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
+import {v2 as cloudinary} from "cloudinary";
 import myHotelsRouter from "./routes/myHotelsRouters";
-import { transporter } from "./utils/emailRelatedUtils/sendVerificationEmail";
+import {transporter} from "./utils/emailRelatedUtils/sendVerificationEmail";
 
 import hotelsRouter from "./routes/hotelsRouter";
 import chatbotRouter from "./routes/chatbotRouter";
 import bookingsRouter from "./routes/bookingsRouter";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const app = express();
 
 app.use(cookieParser()); // * so that we can access the cookies in the request object (refer validateCookie.ts)
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
 // server is going to accept request from client url -->
 app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
 );
 
 transporter.verify((err, success) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Server is ready to take messages");
-  }
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Server is ready to take messages");
+    }
 });
+
+app.get("/health", async (req, res) => {
+    res.send("Health is ok. Server is running");
+});
+
 
 app.use("/api/v1/hotels", hotelsRouter);
 app.use("/api/v1/my-hotels", myHotelsRouter);
@@ -45,7 +50,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/my-bookings", bookingsRouter);
 app.use("/api/v1/chat", chatbotRouter);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
